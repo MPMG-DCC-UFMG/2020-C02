47b6946ab954d89c1ba347a92c138ee68c854442
Leandro <araujoleandroid@gmail.com>
2020-09-28 13:24:40 -0300

    Treating possible input inconsistencies ; memoizing channels by id ; limit keywords results (100) ; allow data-min threshold to channel videos (avoids reaching API daily limit due to single channel with too many videos)

--- youtube/crawler/api.py
+++ youtube/crawler/api.py
@@ -4,8 +4,12 @@ import dateutil.parser
 import pytz
 import time
 import tzlocal
+import urllib
 
 from tqdm import tqdm
+
+
+memoize_channels = {}
 
 
 def username2channels(username, youtube):
@@ -26,12 +30,12 @@ def username2channels(username, youtube):
 
 
 def utc2local(utc_time):
-  '''
-  Thanks to https://stackoverflow.com/a/32904812 for the solution
-  '''
-  local_timezone = tzlocal.get_localzone() # get pytz tzinfo
-  # print(local_timezone)
-  return utc_time.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    '''
+    Thanks to https://stackoverflow.com/a/32904812 for the solution
+    '''
+    local_timezone = tzlocal.get_localzone() # get pytz tzinfo
+    # print(local_timezone)
+    return utc_time.replace(tzinfo=pytz.utc).astimezone(local_timezone)
 
 
 def make_unique_list(mlist):
@@ -73,75 +77,145 @@ def link_to_id(url):
 
 
 class YoutubeCrawlerAPI():
-  def get_video_statistics(self, youtube, video_id):
-    video_analytics_info = youtube.videos().list(
-        id=video_id,
-        part="statistics",
-        maxResults=50
-    ).execute()
-
-    result_dict = {}
-    if("likeCount" in video_analytics_info['items'][0]['statistics']):
-      result_dict['total_likes'] = video_analytics_info['items'][0]['statistics']['likeCount']
-      result_dict['total_dislikes'] = video_analytics_info['items'][0]['statistics']['dislikeCount']
-
-    if('viewCount' in video_analytics_info['items'][0]['statistics']):
-      result_dict['visualizacoes'] = video_analytics_info['items'][0]['statistics']['viewCount']
-    else:
-      result_dict['visualizacoes'] = 0
-
-    return result_dict
-
-  def get_channel_videos(self, youtube, channel_id):
-    final_response_dict = {}
-    channel_profile = {}
-    videos_details = {}
-
-    channel_key = copy.deepcopy(channel_id)
-    if '/' in channel_id:
-        channel_id = channel_id.split('/')[-1]
-        channel_key = channel_key.replace('/', ': ')
-
-    # Channel information
-    request = youtube.channels().list(
-            part='statistics',
-            id=channel_id
-        )
-    subscriber_count = (request.execute())['items'][0]['statistics']['subscriberCount']
-
-    request = youtube.channels().list(
-            part='snippet',
-            id=channel_id
-        )
-    channel_name = (request.execute())['items'][0]['snippet']['title']
-
-    channel_profile['identificador'] = channel_id
-    channel_profile['nome_canal'] = channel_name
-    channel_profile['numero_inscritos'] = subscriber_count
-
-    # Videos information
-    channels_response = youtube.channels().list(part='contentDetails', id=channel_id).execute()
-
-    for channel in channels_response['items']:
-        uploads_list_id = channel["contentDetails"]["relatedPlaylists"]["uploads"]
-        playlistitems_list_request = youtube.playlistItems().list(
-            playlistId=uploads_list_id,
-            part="snippet",
+    def get_video_statistics(self, youtube, video_id):
+        video_analytics_info = youtube.videos().list(
+            id=video_id,
+            part="statistics",
             maxResults=50
-          )
+        ).execute()
 
-        bar = None
-        while playlistitems_list_request:
-            playlistitems_list_response = playlistitems_list_request.execute()
-            if bar is None:
-                bar = tqdm(total=int(playlistitems_list_response['pageInfo']['totalResults']))
-            for playlist_item in playlistitems_list_response["items"]:
-                video_id = playlist_item["snippet"]["resourceId"]["videoId"]
+        result_dict = {}
+        if("likeCount" in video_analytics_info['items'][0]['statistics']):
+            result_dict['total_likes'] = video_analytics_info['items'][0]['statistics']['likeCount']
+            result_dict['total_dislikes'] = video_analytics_info['items'][0]['statistics']['dislikeCount']
 
-                title = playlist_item["snippet"]["title"]
-                description = playlist_item["snippet"]["description"]
-                published_at = playlist_item["snippet"]["publishedAt"]
+        if('viewCount' in video_analytics_info['items'][0]['statistics']):
+            result_dict['visualizacoes'] = video_analytics_info['items'][0]['statistics']['viewCount']
+        else:
+            result_dict['visualizacoes'] = 0
+
+        return result_dict
+
+    def get_channel_videos(self, youtube, channel_id, min_dt=None):
+        global memoize_channels
+
+        final_response_dict = {}
+        channel_profile = {}
+        videos_details = {}
+
+        channel_key = copy.deepcopy(channel_id)
+        if '/' in channel_id:
+            channel_id = channel_id.split('/')[-1]
+            channel_key = channel_key.replace('/', ': ')
+
+        if channel_id not in memoize_channels:
+            # Channel information
+            request = youtube.channels().list(
+                part='statistics',
+                id=channel_id
+            )
+            result = request.execute()
+
+            if 'items' not in result:
+                raise Exception("Error 404: channel id \"%s\" not found" % channel_id)
+
+            if(min_dt is not None):
+                min_dt = (min_dt - datetime.timedelta(minutes=6)).replace(tzinfo=tzlocal.get_localzone()).astimezone(tzlocal.get_localzone())
+
+            subscriber_count = result['items'][0]['statistics']['subscriberCount']
+
+            request = youtube.channels().list(
+                part='snippet',
+                id=channel_id
+            )
+            channel_name = (request.execute())['items'][0]['snippet']['title']
+
+            channel_profile['identificador'] = channel_id
+            channel_profile['nome_canal'] = channel_name
+            channel_profile['numero_inscritos'] = subscriber_count
+
+            # Videos information
+            channels_response = youtube.channels().list(part='contentDetails', id=channel_id).execute()
+
+            for channel in channels_response['items']:
+                uploads_list_id = channel["contentDetails"]["relatedPlaylists"]["uploads"]
+                playlistitems_list_request = youtube.playlistItems().list(
+                    playlistId=uploads_list_id,
+                    part="snippet",
+                    maxResults=50
+                )
+
+                bar = None
+                while playlistitems_list_request:
+                    playlistitems_list_response = playlistitems_list_request.execute()
+
+                    if bar is None:
+                        bar = tqdm(total=int(playlistitems_list_response['pageInfo']['totalResults']))
+                    published_at_dts = []
+
+                    for playlist_item in playlistitems_list_response["items"]:
+                        video_id = playlist_item["snippet"]["resourceId"]["videoId"]
+
+                        title = playlist_item["snippet"]["title"]
+                        description = playlist_item["snippet"]["description"]
+                        published_at = playlist_item["snippet"]["publishedAt"]
+                        published_at_dt = utc2local(dateutil.parser.parse(published_at))
+                        published_at_dts.append(published_at_dt)
+                        # bar.write(str(published_at_dt))
+                    
+                        video_info = {}
+                        video_info['identificador'] = video_id
+                        video_info['titulo'] = title
+                        video_info['descricao'] = description
+                        video_info['data_publicacao'] = published_at
+                        video_info['link_video'] = ('https://www.youtube.com/watch?v=' + video_id)
+
+                        video_statistics = self.get_video_statistics(youtube, video_id)
+
+                        video_info['estatisticas'] = video_statistics
+
+                        if min_dt is None or published_at_dt >= min_dt:
+                            videos_details[video_id] = video_info
+
+                        bar.update(1)
+                
+                    if min_dt is not None and all([ published_at_dt < min_dt for published_at_dt in published_at_dts ]):
+                        break
+
+                    playlistitems_list_request = youtube.playlistItems().list_next(
+                        playlistitems_list_request, playlistitems_list_response
+                    )
+
+                bar.close()
+
+            final_response_dict['informacao_canal'] = channel_profile
+            final_response_dict['informacao_videos'] = videos_details
         
+            memoize_channels[channel_id] = final_response_dict
+
+        return {channel_key: memoize_channels[channel_id]}
+
+
+    def get_videos_by_keyword(self, youtube, keyword, max_results=50):
+        next_video_request = youtube.search().list(
+            part='snippet',
+            type='video',
+            regionCode='BR',
+            q=keyword,
+            maxResults=max_results
+        )
+        
+        videos_details = {}
+        while next_video_request:
+            next_video_request_response = next_video_request.execute()
+            video_results = next_video_request_response["items"]
+
+            for single_video in video_results:
+                video_id = single_video["id"]["videoId"]
+                title = single_video["snippet"]["title"]
+                description = single_video["snippet"]["description"]
+                published_at = single_video["snippet"]["publishedAt"]
+
                 video_info = {}
                 video_info['identificador'] = video_id
                 video_info['titulo'] = title
@@ -150,62 +224,22 @@ class YoutubeCrawlerAPI():
                 video_info['link_video'] = ('https://www.youtube.com/watch?v=' + video_id)
 
                 video_statistics = self.get_video_statistics(youtube, video_id)
-
                 video_info['estatisticas'] = video_statistics
                 videos_details[video_id] = video_info
 
-                bar.update(1)
+            if len(videos_details) >= max_results:
+                break
 
-            playlistitems_list_request = youtube.playlistItems().list_next(
-                playlistitems_list_request, playlistitems_list_response
+            next_video_request = youtube.search().list_next(
+                next_video_request, next_video_request_response
             )
 
-        bar.close()
+        return videos_details
 
-    final_response_dict['informacao_canal'] = channel_profile
-    final_response_dict['informacao_videos'] = videos_details
+    def get_video_comments(self, youtube, video_id, max_comments, min_dt, max_dt):
+        # print(youtube.captions().download(id=video_id).execute())
 
-    return {channel_key: final_response_dict}
-
-  def get_videos_by_keyword(self, youtube, keyword):
-    next_video_request = youtube.search().list(
-            part='snippet',
-            type='video',
-            regionCode='BR',
-            q=keyword,
-            maxResults=50
-        )
-    
-    videos_details = {}
-    while next_video_request:
-      next_video_request_response = next_video_request.execute()
-      video_results = next_video_request_response["items"]
-
-      for single_video in video_results:
-        video_id = single_video["id"]["videoId"]
-        title = single_video["snippet"]["title"]
-        description = single_video["snippet"]["description"]
-        published_at = single_video["snippet"]["publishedAt"]
-
-        video_info = {}
-        video_info['identificador'] = video_id
-        video_info['titulo'] = title
-        video_info['descricao'] = description
-        video_info['data_publicacao'] = published_at
-        video_info['link_video'] = ('https://www.youtube.com/watch?v=' + video_id)
-
-        video_statistics = self.get_video_statistics(youtube, video_id)
-        video_info['estatisticas'] = video_statistics
-        videos_details[video_id] = video_info
-
-      next_video_request = youtube.search().list_next(
-          next_video_request, next_video_request_response
-      )
-
-    return videos_details
-
-  def get_video_comments(self, youtube, video_id, max_comments, min_dt, max_dt):
-    next_video_request = youtube.commentThreads().list(
+        next_video_request = youtube.commentThreads().list(
             part='snippet',
             videoId=video_id,
             maxResults=50,
@@ -213,68 +247,69 @@ class YoutubeCrawlerAPI():
             # order='relevance' # order='time'
         )
 
-    if(min_dt is not None):
-      min_dt = (min_dt - datetime.timedelta(minutes=6)).replace(tzinfo=tzlocal.get_localzone()).astimezone(tzlocal.get_localzone())
-    if(max_dt is not None):
-      max_dt = (max_dt - datetime.timedelta(minutes=6)).replace(tzinfo=tzlocal.get_localzone()).astimezone(tzlocal.get_localzone())
+        if(min_dt is not None):
+            min_dt = (min_dt - datetime.timedelta(minutes=6)).replace(tzinfo=tzlocal.get_localzone()).astimezone(tzlocal.get_localzone())
+        if(max_dt is not None):
+            max_dt = (max_dt - datetime.timedelta(minutes=6)).replace(tzinfo=tzlocal.get_localzone()).astimezone(tzlocal.get_localzone())
 
-    comments = {}
-    oldest_published_dt = datetime.datetime.now() + datetime.timedelta(days=360)
-    still_collecting = True
-    while(next_video_request and still_collecting):
-      next_video_request_response = next_video_request.execute()
+        comments = {}
+        oldest_published_dt = datetime.datetime.now() + datetime.timedelta(days=360)
+        still_collecting = True
+        while(next_video_request and still_collecting):
+            next_video_request_response = next_video_request.execute()
 
-      # del next_video_request_response['items']
-      # print(next_video_request_response)
+            # del next_video_request_response['items']
+            # print(next_video_request_response)
 
-      video_results = next_video_request_response["items"]
+            video_results = next_video_request_response["items"]
 
-      for single_comment in video_results:
-        comment_id = single_comment["id"]
+            for single_comment in video_results:
+                comment_id = single_comment["id"]
 
-        if(single_comment["snippet"]['topLevelComment']):
-          text = single_comment["snippet"]['topLevelComment']["snippet"]["textDisplay"]
-          published_at = single_comment["snippet"]['topLevelComment']["snippet"]["publishedAt"]
-          author_name = single_comment["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
-          author_id = single_comment["snippet"]['topLevelComment']["snippet"]["authorChannelId"]['value']
-          like_count = single_comment["snippet"]['topLevelComment']["snippet"]["likeCount"]
-        else:
-          text = single_comment["snippet"]["textDisplay"]
-          published_at = single_comment["snippet"]["publishedAt"]
-          author_id = single_comment["snippet"]["authorChannelId"]['value']
-          author_name = single_comment["snippet"]["authorDisplayName"]
-          like_count = single_comment["snippet"]["likeCount"]
-        published_at_dt = utc2local(dateutil.parser.parse(published_at))
+                if(single_comment["snippet"]['topLevelComment']):
+                    text = single_comment["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+                    published_at = single_comment["snippet"]['topLevelComment']["snippet"]["publishedAt"]
+                    author_name = single_comment["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
+                    author_id = single_comment["snippet"]['topLevelComment']["snippet"]["authorChannelId"]['value']
+                    like_count = single_comment["snippet"]['topLevelComment']["snippet"]["likeCount"]
+                else:
+                    text = single_comment["snippet"]["textDisplay"]
+                    published_at = single_comment["snippet"]["publishedAt"]
+                    author_id = single_comment["snippet"]["authorChannelId"]['value']
+                    author_name = single_comment["snippet"]["authorDisplayName"]
+                    like_count = single_comment["snippet"]["likeCount"]
+                published_at_dt = utc2local(dateutil.parser.parse(published_at))
 
-        comment_info = {}
-        comment_info['id_video'] = video_id
-        comment_info['id_comentario'] = comment_id
-        comment_info['texto'] = text
-        comment_info['id_autor'] = author_id
-        comment_info['nome_autor'] = author_name
-        comment_info['numero_likes'] = like_count
-        comment_info['data_publicacao'] = published_at
+                comment_info = {}
+                comment_info['id_video'] = video_id
+                comment_info['id_comentario'] = comment_id
+                comment_info['texto'] = text
+                comment_info['id_autor'] = author_id
+                comment_info['nome_autor'] = author_name
+                comment_info['numero_likes'] = like_count
+                comment_info['data_publicacao'] = published_at
 
-        oldest_published_dt = copy.deepcopy(published_at_dt)
+                oldest_published_dt = copy.deepcopy(published_at_dt)
 
-        sat_min_date = min_dt is None or published_at_dt >= min_dt
-        sat_max_date = max_dt is None or published_at_dt < max_dt
-        # print(published_at_dt, min_dt, sat_min_date)
-        if sat_min_date and sat_max_date:
-          comments[comment_id] = comment_info
-          # print('>> added')
+                sat_min_date = min_dt is None or published_at_dt >= min_dt
+                sat_max_date = max_dt is None or published_at_dt < max_dt
+                sat_max_comments = max_comments is None or len(comments) < max_comments
+                # print(published_at_dt, min_dt, sat_min_date)
+                if sat_min_date and sat_max_date and sat_max_comments:
+                    comments[comment_id] = comment_info
+                    # print('>> added')
 
-        # print(published_at)
-        # time.sleep(0.25)
-        # print(comment_info)
-      
-      if(max_comments is not None and len(comments) >= max_comments):
-        still_collecting = False
-      elif(oldest_published_dt < min_dt):
-        still_collecting = False
-      
-      next_video_request = youtube.commentThreads().list_next(
-          next_video_request, next_video_request_response
-      )
+                # print(published_at)
+                # time.sleep(0.25)
+                # print(comment_info)
+            
+            if(max_comments is not None and len(comments) >= max_comments):
+                still_collecting = False
+            elif(oldest_published_dt < min_dt):
+                still_collecting = False
+            
+            next_video_request = youtube.commentThreads().list_next(
+                next_video_request, next_video_request_response
+            )
 
-    return comments
+        return comments
--- youtube/main.py
+++ youtube/main.py
@@ -6,7 +6,6 @@ import dateutil.parser
 import json
 import os
 import time
-import urllib
 
 from googleapiclient.discovery import build
 from sys import argv
@@ -19,6 +18,7 @@ def main(input_json_folder):
     target_json_folder = '/var/youtube-crawler/jsons/'
     if not os.path.isdir(target_json_folder):
         os.mkdir(target_json_folder)
+    output_file = os.path.join(target_json_folder, (str(int(time.time()*1000)) + '.json'))
 
     api = crawler.api.YoutubeCrawlerAPI()
     api_keys = []
@@ -55,72 +55,89 @@ def main(input_json_folder):
         channels_container = crawler.api.make_unique_list(list(map(crawler.api.link_to_id, data['id_canais_youtube']))) if 'id_canais_youtube' in data else []
         videos = crawler.api.make_unique_list(list(map(crawler.api.link_to_id, data['id_videos_youtube']))) if 'id_videos_youtube' in data else []
         keywords = crawler.api.make_unique_list(data['palavras']) if 'palavras' in data else []
-        max_comments = int(data['max_comentarios']) if 'max_comentarios' in data else None
-        data_min = dateutil.parser.parse(data['data_min']) if 'data_min' in data else None
-        data_max = dateutil.parser.parse(data['data_max']) if 'data_max' in data else None
+        max_commnets, data_min, data_max = None, None, None
 
-        youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
+        try:
+            max_comments = int(data['max_comentarios']) if 'max_comentarios' in data else None
+            data_min = dateutil.parser.parse(data['data_min']) if 'data_min' in data else None
+            data_max = dateutil.parser.parse(data['data_max']) if 'data_max' in data else None
+        except Exception as e:
+            final_dict[ERROR_KEY] = 'Erro de inicialização: ' + str(e).strip()
 
-        channels_info = []
-        for channel_name in channels_container:
-            profile_channels = crawler.api.username2channels(channel_name, youtube)
-            channels = [ channel_name ] if not profile_channels else [ ('%s/%s' % (channel_name, channel_id)) for channel_id in profile_channels ]
+        if ERROR_KEY not in final_dict:
+            youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
 
-            for channel in channels:
+            channels_info = {}
+            for channel_name in channels_container:
+                profile_channels = crawler.api.username2channels(channel_name, youtube)
+                channels = [ channel_name ] if not profile_channels else [ ('%s/%s' % (channel_name, channel_id)) for channel_id in profile_channels ]
+
+                for channel in channels:
+                    still_collecting = True
+                    while(api_key_usage != len(api_keys) and still_collecting):
+                        try:
+                            channels_info.update(api.get_channel_videos(youtube, channel, data_min))
+                            still_collecting = False
+                        except Exception as e:
+                            if 'error 404' in str(e).lower():
+                                channels_info[channel] = { ERROR_KEY: 'canal não encontrado' }
+                                still_collecting = False
+                            else:
+                                api_key_usage += 1
+                                if(api_key_usage < (len(api_keys) - 1)):
+                                    youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
+                                print(str(e))
+
+            videos_info = {}
+            api_key_usage = -1
+            still_collecting = True
+            for video in videos:
                 still_collecting = True
                 while(api_key_usage != len(api_keys) and still_collecting):
                     try:
-                        channels_info.append(api.get_channel_videos(youtube, channel))
+                        videos_info[video] = api.get_video_comments(youtube, video, max_comments, data_min, data_max)
                         still_collecting = False
-                    except:
+                    except Exception as e:
+                        if 'error 404' in str(e).lower():
+                            videos_info[video] = { ERROR_KEY: 'video não encontrado' }
+                            still_collecting = False
+                        else:
+                            api_key_usage += 1
+                            if(api_key_usage < (len(api_keys) - 1)):
+                                youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
+                            print('problema na coleta: %s' % str(e))
+
+            keywords_info = {}
+            api_key_usage = -1
+            still_collecting = True
+            for keyword in keywords:
+                still_collecting = True
+                while(api_key_usage != len(api_keys) and still_collecting):
+                    try:
+                        keywords_info[keyword] = api.get_videos_by_keyword(youtube, keyword) if keyword.strip() else {}
+                        still_collecting = False
+                    except Exception as e:
                         api_key_usage += 1
                         if(api_key_usage < (len(api_keys) - 1)):
                             youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
+                        print('problema na coleta: %s' % str(e))
 
-        videos_info = []
-        api_key_usage = -1
-        still_collecting = True
-        for video in videos:
-            still_collecting = True
-            while(api_key_usage != len(api_keys) and still_collecting):
-                try:
-                    videos_info.append(api.get_video_comments(youtube, video, max_comments, data_min, data_max))
-                    still_collecting = False
-                except Exception as e:
-                    api_key_usage += 1
-                    if(api_key_usage < (len(api_keys) - 1)):
-                        youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
-                    print('problema na coleta: %s' % str(e))
 
-        keywords_info = []
-        api_key_usage = -1
-        still_collecting = True
-        for keyword in keywords:
-            still_collecting = True
-            while(api_key_usage != len(api_keys) and still_collecting):
-                try:
-                    keywords_info.append(api.get_videos_by_keyword(youtube, keyword))
-                    still_collecting = False
-                except:
-                    api_key_usage += 1
-                    if(api_key_usage < (len(api_keys) - 1)):
-                        youtube = build('youtube', 'v3', developerKey=api_keys[api_key_usage])
+            if(len(keywords) == 0): still_collecting = False
+            # final_dict = dict()
+            final_dict['informacoes_canal_youtube'] = channels_info
+            final_dict['comentarios_video_youtube'] = videos_info
+            final_dict['videos_por_keyword_youtube'] = keywords_info
 
-        if(len(keywords) == 0): still_collecting = False
-        # final_dict = dict()
-        final_dict['informacoes_canal_youtube'] = channels_info
-        final_dict['comentarios_video_youtube'] = videos_info
-        final_dict['videos_por_keyword_youtube'] = keywords_info
+            if(still_collecting): print("\n\nAVISO!\n\nA quantidade diária de coletas permitida nas API Keys informadas foi atingida, portanto alguns dados não puderam ser coletados. Volte amanhã ou insira novas API Keys para coletar mais dados.\n\n")
+            else:print("\n\nDados salvos com sucesso!\n\n")
 
-        if(still_collecting): print("\n\nAVISO!\n\nA quantidade diária de coletas permitida nas API Keys informadas foi atingida, portanto alguns dados não puderam ser coletados. Volte amanhã ou insira novas API Keys para coletar mais dados.\n\n")
-        else:print("\n\nDados salvos com sucesso!\n\n")
-
-    output_file = os.path.join(target_json_folder, (str(int(time.time()*1000)) + '.json'))
     # output_file = os.path.join(data['pasta_da_saida'], (str(int(time.time()*1000)) + '.json'))
     with open(output_file, 'w') as outfile:
         json.dump(final_dict, outfile)
 
     print(json.dumps(final_dict))
+
 
 input_json_folder = argv[1]
 main(argv[1])
