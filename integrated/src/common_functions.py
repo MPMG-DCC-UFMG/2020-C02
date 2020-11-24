279c5ac1e39c359cef17d0fb13d8f1bb2686f4af
Josemar Caetano <josemar.caetano@hotmail.com>
2020-11-24 17:30:41 -0300

    Adaptação da coleta Instagram para escrita KAFKA.

--- integrated/src/common_functions.py
+++ integrated/src/common_functions.py
@@ -6,15 +6,16 @@ import time
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
+import sys
 
 # MESSAGES = [ str(datetime.datetime.now()), 'Hello,', 'World' ]
-KAFKA_SERVERS = [ ('hadoopdn-gsi-prod0' + str(j) + '.mpmg.mp.br:6667').replace('010', '10') for j in range(4, 10+1) ]
+KAFKA_SERVERS = [('hadoopdn-gsi-prod0' + str(j) + '.mpmg.mp.br:6667').replace('010', '10') for j in range(4, 10 + 1)]
 KAFKA_SERVERS = KAFKA_SERVERS[:1]
 LIBS = {}
-ROWS_INFO_ATOMIC = [ l.strip() for l in open('../data/info_atomic.csv', 'rt') if l.strip() ]
-ROWS_INFO_HIGH = [ l.strip() for l in open('../data/info_high.csv', 'rt') if l.strip() ]
-ROWS_INFO_HIGH2ATOMIC = [ l.strip() for l in open('../data/info_high2atomic.csv', 'rt') if l.strip() ]
-ROWS_INFO_TOPICS = [ l.strip() for l in open('../data/info_topics.csv', 'rt') if l.strip() ]
+# ROWS_INFO_ATOMIC = [ l.strip() for l in open('../data/info_atomic.csv', 'rt') if l.strip() ]
+ROWS_INFO_HIGH = [l.strip() for l in open('../data/info_high.csv', 'rt') if l.strip()]
+ROWS_INFO_HIGH2ATOMIC = [l.strip() for l in open('../data/info_high2atomic.csv', 'rt') if l.strip()]
+ROWS_INFO_TOPICS = [l.strip() for l in open('../data/info_topics.csv', 'rt') if l.strip()]
 
 
 def get_allowed_social_nets():
@@ -50,20 +51,29 @@ def crawl_atomic(atomic_request):
 
     function_name = None
     for row in ROWS_INFO_HIGH2ATOMIC:
-        this_net, this_high_mode, this_atomic, this_respective_function = list(map(lambda x: x.strip(), row.strip().lower().split(',')))
+        this_net, this_high_mode, this_atomic, this_respective_function = list(
+            map(lambda x: x.strip(), row.strip().lower().split(',')))
         if this_net == net and mode == this_high_mode and which == this_atomic:
             function_name = copy.deepcopy(this_respective_function)
 
     if function_name is not None:
-        # social_media, 
-        LIBS[net].shell(json.dumps(js), mode)
-        getattr(LIBS[net], function_name)(value, crawling_id)
+        # social_media,
+
+        coletor = None
+        try:
+            coletor = LIBS[net].get_coletor_object(js)
+        except Exception as e:
+            LIBS[net].shell(json.dumps(js), mode)
+            coletor = LIBS[net]
+
+        getattr(coletor, function_name)(value, crawling_id)
+
     else:
         print('not prepared to deal with the following request: %s' % json.dumps(atomic_request))
         exit(0)
 
     return
-   
+
 
 def publish_kafka_message(producer_instance, topic_name, key, value):
     '''
@@ -93,6 +103,10 @@ def connect_kafka_producer():
     global KAFKA_SERVERS
 
     _producer = None
+
+    return _producer
+    ### XXX TODO
+
     try:
         _producer = KafkaProducer(bootstrap_servers=KAFKA_SERVERS, api_version=(0, 10))
         if not _producer.bootstrap_connected():
@@ -116,9 +130,9 @@ def get_social_network_topic(net):
 
 
 def read_next_atomic_level_from_kafka():
-    topics = [ get_social_network_topic(net) for net in get_allowed_social_nets() ]
+    topics = [get_social_network_topic(net) for net in get_allowed_social_nets()]
     for topic in topics:
-        mstr = None # XXX read_kafka_next_row(topic)
+        mstr = None  # XXX read_kafka_next_row(topic)
         if mstr is not None:
             return mstr
 
@@ -136,12 +150,14 @@ def add_low_level_requests_to_kafka(atomic_requests):
             js = json.loads(js)
 
         topic = get_social_network_topic(net)
-        instance = json.dumps([ net, mode, value, which, js ])
+        instance = json.dumps([net, mode, value, which, js])
         print(instance)
+        # print(producer_instance)
+        # sys.exit(0)
         sent = publish_kafka_message(producer_instance, topic, 'raw', 'test msg XXX')
-        sent = 1 # XXX publish_kafka_message(producer_instance, topic, 'raw', instance)
+        sent = 1  # XXX publish_kafka_message(producer_instance, topic, 'raw', instance)
         if sent:
-            added_ones.append(json.dumps([ net ] + json.loads(instance)[:-1]))
+            added_ones.append(json.dumps([net] + json.loads(instance)[:-1]))
 
     return added_ones
 
@@ -160,7 +176,7 @@ def get_high_level_requests(js, net):
             for element in js[this_mode]:
                 element = element.strip()
                 if element not in already:
-                    high_requests.append(( net, this_mode, element, js ))
+                    high_requests.append((net, this_mode, element, js))
                     already.add(element)
 
     return high_requests
@@ -195,6 +211,11 @@ def has_available_credentials(js, net):
                 pass
             if api_object is not None:
                 return True
+    else:
+        if LIBS[net].does_not_need_credential(js):
+            return True
+
+        return LIBS[net].authenticate(js) is not None
 
     return False
 
---
+++ integrated/src/instagram_data_handle.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+
+import sys
+import os
+from datetime import datetime
+
+import json
+
+class DataHandle:
+    """
+    Contem metodos para persistir e recuperar dados, criar diretorios e formatar string de data-e-hora.
+    Atributos
+    ----------
+    Metodos
+    -------
+    persistData(filename_output: str, document_list: list de str, operation_type: 'w' para write ou 'a' para append)
+        Persite uma lista de documentos em um arquivo (filename_output) ou em outro meio apropriado.
+    getData(filename_input: str, attributes_to_select: list de str)
+        Retorna lista de documentos no arquivo (ou local) filename_input.
+        Cada documento contem somente os atributos especificados em attributes_to_select
+    create_directories(directories_list: list de str)
+        Cria diretorios especificados em directories_list
+    getDateFormatted(string_datetime: str, only_date: bool)
+        Formata uma string de data e hora. Retorna data e hora ou somente a data de acordo com only_date
+    """
+    def __init__(self):
+        self.profile_post_info_list = []
+        self.post_info_list = []
+        self.comment_info_list = []
+        self.profile_comment_info_list = []
+
+        self.unified_documents_list = []
+
+
+    ### TODO Adaptar KAFKA
+    def persistData(self, filename_output, document_list, data_topic, crawling_id, operation_type=None):
+        ### GRAVA EM ARQUIVO
+        ## self.__updateDataFile(filename_output=filename_output, document_list=document_list, operation_type=operation_type)
+        ### Salva em memoria e grava no KAFKA
+        if "profiles_posts.json" in filename_output:
+            self.profile_post_info_list.extend(document_list)
+            #### (TODO) GRAVA no KAFKA
+
+        elif "posts.json" in filename_output:
+            self.post_info_list.extend(document_list)
+        elif "profiles_comments.json" in filename_output:
+            self.profile_comment_info_list.extend(document_list)
+        elif "comments.json" in filename_output:
+            self.comment_info_list.extend(document_list)
+        else:
+            ### TODO apos executar essa linha, devo alterar o status da coleta para finalizado
+            self.unified_documents_list.extend(document_list)
+
+
+    def __updateDataFile(self,filename_output, document_list, operation_type):
+        with open(filename_output, operation_type, encoding='utf-8') as file_output:
+            for document in document_list:
+                if document is not None:
+                    json.dump(document, file_output)
+                    file_output.write("\n")
+                    file_output.flush()
+
+
+    def getData(self, filename_input, attributes_to_select=None, document_type = None):
+        ### Recupera de ARQUIVO
+        ## return self.__getDataFromFile(filename_input=filename_input, attributes_to_select=attributes_to_select, document_type=document_type)
+        ### Recupera de Memoria
+        return self.__getDataFromMemory(filename_input=filename_input, attributes_to_select=attributes_to_select,
+                                      document_type=document_type)
+
+    def __getDataFromMemory(self, filename_input, document_type, attributes_to_select=None):
+        document_list = []
+
+        input_document_list = self.profile_post_info_list if "profiles_posts.json" in filename_input else (self.post_info_list if "posts.json" in filename_input else self.comment_info_list)
+
+        for document_input in input_document_list:
+            if document_type is None or (document_type is not None and document_input["tipo_documento"] == document_type):
+                document_output = {}
+                if attributes_to_select is not None and len(attributes_to_select) > 0:
+                    for attribute_name in attributes_to_select:
+                        document_output[attribute_name] = document_input[attribute_name]
+                else:
+                    document_output = document_input
+
+                document_list.append(document_output)
+
+        return(document_list)
+
+
+
+    def __getDataFromFile(self, filename_input, attributes_to_select=None, document_type=None):
+        document_list = []
+
+        if (os.path.exists(filename_input) is True):
+            with open(filename_input, "r", encoding='utf-8') as file_input:
+                for document in file_input:
+                    document_input = json.loads(document)
+
+                    if document_type is None or (document_type is not None and document_input["tipo_documento"] == document_type):
+                        document_output = {}
+                        if attributes_to_select is not None and len(attributes_to_select) > 0:
+                            for attribute_name in attributes_to_select:
+                                document_output[attribute_name] = document_input[attribute_name]
+                        else:
+                            document_output = document_input
+
+                        document_list.append(document_output)
+
+        return(document_list)
+
+    def create_directories(self,directories_list):
+        for directory in directories_list:
+            # print("Creating directory", directory, flush=True)
+            try:
+                os.mkdir(directory)
+            except FileExistsError:
+                pass
+
+    def getDateFormatted(self, string_datetime, only_date=False):
+        a_datetime = None
+        string_datetime = string_datetime.replace("\"", "")
+        string_datetime = string_datetime.replace("'", "")
+        if only_date is True:
+            try:
+                a_datetime = datetime.strptime(string_datetime, '%Y-%m-%d')
+            except Exception as e:
+                try:
+                    a_datetime = datetime.strptime(string_datetime, '%d-%m-%Y')
+                except Exception as e:
+                    print("Erro ao formatar data do tipo:", string_datetime, "Erro detalhado:", e, flush=True)
+        else:
+            try:
+                a_datetime = datetime.strptime(string_datetime, '%Y-%m-%d %H:%M:%S')
+            except Exception as e:
+                try:
+                    a_datetime = datetime.strptime(string_datetime, '%Y-%m-%d %H:%M:%S.%f')
+                except Exception as e:
+                    try:
+                        a_datetime = datetime.strptime(string_datetime, '%Y-%m-%dT%H:%M:%SZ')
+                    except Exception as e:
+                        try:
+                            a_datetime = datetime.strptime(string_datetime, '%a %b %d %H:%M:%S +0000 %Y')
+                        except Exception as e:
+                            try:
+                                a_datetime = datetime.strptime(string_datetime, '%d-%m-%Y %H:%M:%S')
+                            except Exception as e:
+                                print("Erro ao formatar data do tipo:", string_datetime,
+                                      "\tNao foi possivel identificar um padrao na string.", flush=True)
+        return (a_datetime)
+
