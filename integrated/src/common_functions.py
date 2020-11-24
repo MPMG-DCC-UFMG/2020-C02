3d64ab3e4689a65b64939c38c13b1618b2c1c5e7
16leandrosilva <araujoleandroid@gmail.com>
2020-11-24 13:55:58 -0300

    configuration files to make inegration easier and more generic

--- integrated/src/common_functions.py
+++ integrated/src/common_functions.py
@@ -1,5 +1,7 @@
+import copy
+import importlib
 import json
-import twitter_api
+import time
 
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
@@ -8,10 +10,26 @@ from kafka import KafkaProducer
 # MESSAGES = [ str(datetime.datetime.now()), 'Hello,', 'World' ]
 KAFKA_SERVERS = [ ('hadoopdn-gsi-prod0' + str(j) + '.mpmg.mp.br:6667').replace('010', '10') for j in range(4, 10+1) ]
 KAFKA_SERVERS = KAFKA_SERVERS[:1]
+LIBS = {}
+ROWS_INFO_ATOMIC = [ l.strip() for l in open('../data/info_atomic.csv', 'rt') if l.strip() ]
+ROWS_INFO_HIGH = [ l.strip() for l in open('../data/info_high.csv', 'rt') if l.strip() ]
+ROWS_INFO_HIGH2ATOMIC = [ l.strip() for l in open('../data/info_high2atomic.csv', 'rt') if l.strip() ]
+ROWS_INFO_TOPICS = [ l.strip() for l in open('../data/info_topics.csv', 'rt') if l.strip() ]
 
 
 def get_allowed_social_nets():
-    return frozenset([ 'twitter' ])
+    global LIBS
+    global ROWS_INFO_TOPICS
+
+    nets = []
+    for row in ROWS_INFO_TOPICS:
+        net = row.split(',')[0].strip().lower()
+        if net != 'network':
+            nets.append(net)
+            if net not in LIBS:
+                LIBS[net] = importlib.import_module(net + '_api')
+
+    return frozenset(nets)
 
 
 def get_error_key():
@@ -19,32 +37,33 @@ def get_error_key():
 
 
 def crawl_atomic(atomic_request):
+    global LIBS
+    global ROWS_INFO_HIGH2ATOMIC
+
+    crawling_id = str(int(round(time.time() * 1000)))
     if type(atomic_request) != tuple and type(atomic_request) != list:
         atomic_request = json.loads(atomic_request)
 
-    net, container, value, js, which = atomic_request
+    net, mode, value, which, js = atomic_request
+    if net not in LIBS:
+        _ = get_allowed_social_nets()
 
-    finished = False
-    if net == 'twitter':
-        if which == 'posts':
-            twitter_api.download_posts()
-            finished = True
-        elif which == 'perfil':
-            XXX
-            finished = True
-        elif which == 'seguidores':
-            XXX
-            finished = True
-        elif which == 'seguindo':
-            XXX
-            finished = True
+    function_name = None
+    for row in ROWS_INFO_HIGH2ATOMIC:
+        this_net, this_high_mode, this_atomic, this_respective_function = list(map(lambda x: x.strip(), row.strip().lower().split(',')))
+        if this_net == net and mode == this_high_mode and which == this_atomic:
+            function_name = copy.deepcopy(this_respective_function)
 
-    if not finished:
+    if function_name is not None:
+        # social_media, 
+        LIBS[net].shell(json.dumps(js), mode)
+        getattr(LIBS[net], function_name)(value, crawling_id)
+    else:
         print('not prepared to deal with the following request: %s' % json.dumps(atomic_request))
+        exit(0)
 
     return
    
-
 
 def publish_kafka_message(producer_instance, topic_name, key, value):
     '''
@@ -86,18 +105,20 @@ def connect_kafka_producer():
 
 
 def get_social_network_topic(net):
-    if net == 'twitter':
-        return 'crawler_twitter'
+    global ROWS_INFO_TOPICS
+
+    for row in ROWS_INFO_TOPICS:
+        this_net = row.split(',')[0].strip().lower()
+        if net == this_net:
+            return row.split(',')[1].strip().lower()
 
     return None
 
 
 def read_next_atomic_level_from_kafka():
-    XXX = YYY
-
-    topics = [ 'crawler_twitter', 'crawler_youtube' ]
+    topics = [ get_social_network_topic(net) for net in get_allowed_social_nets() ]
     for topic in topics:
-        mstr = read_kafka_next_row(topic)
+        mstr = None # XXX read_kafka_next_row(topic)
         if mstr is not None:
             return mstr
 
@@ -105,20 +126,20 @@ def read_next_atomic_level_from_kafka():
 
 
 def add_low_level_requests_to_kafka(atomic_requests):
-    # atomic_requests.append((net, container, value, json.dumps(js), 'seguidores'))
+    # atomic_requests.append((net, mode, value, json.dumps(js), 'seguidores'))
 
     added_ones = []
     producer_instance = connect_kafka_producer()
 
-    print(atomic_requests[0])
-    for net, container, value, js, which in atomic_requests:
+    for net, mode, value, js, which in atomic_requests:
         if type(js) != dict:
             js = json.loads(js)
 
         topic = get_social_network_topic(net)
-        instance = json.dumps([ container, value, which, js ])
+        instance = json.dumps([ net, mode, value, which, js ])
+        print(instance)
         sent = publish_kafka_message(producer_instance, topic, 'raw', 'test msg XXX')
-        sent = 1 # XXX
+        sent = 1 # XXX publish_kafka_message(producer_instance, topic, 'raw', instance)
         if sent:
             added_ones.append(json.dumps([ net ] + json.loads(instance)[:-1]))
 
@@ -126,53 +147,54 @@ def add_low_level_requests_to_kafka(atomic_requests):
 
 
 def get_high_level_requests(js, net):
+    global ROWS_INFO_HIGH
+
     high_requests = []
 
-    if net == 'twitter':
-        for container in [ 'users', 'words' ]:
+    for row in ROWS_INFO_HIGH:
+        this_net, this_mode = row.lower().split(',')
+        this_net, this_mode = this_net.strip(), this_mode.strip()
+
+        if this_net == net and this_mode in js:
             already = set()
-            if container in js:
-                for element in js[container]:
-                    element = element.strip()
-                    if element not in already:
-                        high_requests.append(( net, container, element, js ))
-                        already.add(element)
+            for element in js[this_mode]:
+                element = element.strip()
+                if element not in already:
+                    high_requests.append(( net, this_mode, element, js ))
+                    already.add(element)
 
     return high_requests
 
 
 def high_to_atomic_level(high_request):
+    global ROWS_INFO_HIGH2ATOMIC
+
     atomic_requests = []
 
-    net, container, value, js = high_request
+    net, mode, value, js = high_request
     if type(js) != dict:
         js = json.loads(js)
 
-    if net == 'twitter':
-        if container in [ 'users', 'usuarios' ]:
-            atomic_requests.append((net, container, value, json.dumps(js), 'perfil'))
-            atomic_requests.append((net, container, value, json.dumps(js), 'seguidores'))
-            atomic_requests.append((net, container, value, json.dumps(js), 'seguindo'))
-            atomic_requests.append((net, container, value, json.dumps(js), 'posts'))
-        elif container in [ 'words', 'keywords', 'palavras' ]:
-            atomic_requests.append((net, container, value, json.dumps(js), 'posts'))
+    for row in ROWS_INFO_HIGH2ATOMIC:
+        this_net, this_high_mode, this_atomic, _ = list(map(lambda x: x.strip(), row.strip().lower().split(',')))
+        if net == this_net and mode == this_high_mode:
+            atomic_requests.append((net, mode, value, json.dumps(js), this_atomic))
 
     return atomic_requests
 
 
 def has_available_credentials(js, net):
-    if net == 'twitter':
-        if 'tokens' in js:
-            for t in js['tokens']:
-                api_object = None
-                try:
-                    api_object = twitter_api.credentials_to_api_object(t)
-                except:
-                    pass
-                if api_object is not None:
-                    return True
+    global LIBS
 
-        return False
+    if 'tokens' in js:
+        for t in js['tokens']:
+            api_object = None
+            try:
+                api_object = LIBS[net].credentials_to_api_object(t)
+            except:
+                pass
+            if api_object is not None:
+                return True
 
     return False
 
--- integrated/src/hybrid.py
+++ integrated/src/hybrid.py
@@ -1,5 +1,6 @@
 import common_functions as common
 import json
+import sys
 import time
 
 
@@ -13,9 +14,13 @@ if __name__ == '__main__':
         read_from_kafka_and_run_a_single_atomic_level_request()
     '''
 
-    while True:
-        atomic_level_request = common.read_next_atomic_level_from_kafka()
-        if atomic_level_request is not None:
-            common.crawl_atomic(atomic_level_request)
-        
-        time.sleep(SLEEPING_SECONDS)
+    if '-j' in sys.argv:
+        atomic_level_request = json.loads([ l.strip() for l in open('../data/my_atomic_instance.txt', 'rt') if l.strip() ][0])
+        common.crawl_atomic(atomic_level_request)
+    else:
+        while True:
+            atomic_level_request = common.read_next_atomic_level_from_kafka()
+            if atomic_level_request is not None:
+                common.crawl_atomic(atomic_level_request)
+            
+            time.sleep(SLEEPING_SECONDS)
