93fde7abef3f50ff670512ab9ac1ff4ca0c588d7
Marcelo Mascarenhas <70808580+marcelo-mascarenhas@users.noreply.github.com>
2021-08-18 15:23:49 -0300

    Delete twitter directory

--- twitter/twitter_downloader.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-
-for f in ${BASH_ARGV[*]} ; do
-    { docker run -v "$(python3 json_maker.py $f --output-folder):/var/twitter-crawler/" --rm -it $(docker images | grep 'mp_tt' | head -1 | cut -d' ' -f 1) python3 /home/mp/coletor-twitter/main.py -d "$(python3 json_maker.py $f)" ; }
-    # -e LANG=C.UTF-8
-done
